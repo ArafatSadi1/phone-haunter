3cb405d481938dc3f3f2d515efb1d200e3c65c92
ArafatSadi1 <arafatsadi1@gmail.com>
2022-03-02 18:40:38 +0600

    style add

--- JS/script.js
+++ JS/script.js
@@ -2,26 +2,36 @@ const searchPhone = () => {
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
     // console.log(searchText);
+    if(searchText === 'oppo' || searchText === 'samsung' || searchText === 'iphone'){
+        const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}
+        `
+        fetch(url)
+        .then(res => res.json())
+        .then(data => displayResult(data.data));
+        document.getElementById('error').style.display = 'none';
+    }
+    else{
+        const searchResult = document.getElementById('search-result')
+        // clear search result
+        searchResult.textContent = '';
+        document.getElementById('error').style.display = 'block';      
+    }
     searchField.value = '';
-    const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}
-    `
-    fetch(url)
-    .then(res => res.json())
-    .then(data => searchResult(data.data));
 }
 
-const searchResult = phones => {
+const displayResult = phones => {
     const searchResult = document.getElementById('search-result')
     // clear search result
     searchResult.textContent = '';
     phones.forEach( phone => {
+        console.log(phone)
         const div = document.createElement('div')
         div.classList.add('col')
         div.innerHTML = `
-        <div class="card h-100">
+        <div class="card m-2 p-4 h-80">
         <img src="${phone.image}" class="card-img-top" alt="...">
         <div class="card-body">
-        <h5 class="card-title">${phone.phone_name}</h5>
+        <h3 class="card-title">${phone.phone_name}</h3>
         <button onclick="loadSingleResult('${phone.slug}')">Details</button>
         </div>
         </div>
@@ -34,10 +44,10 @@ const loadSingleResult = (phone) => {
     const url = `https://openapi.programming-hero.com/api/phone/${phone}`
     fetch(url)
     .then(res => res.json())
-    .then(data => displaySingleMeal(data.data))
+    .then(data => displaySinglePhone(data.data))
 }
 
-const displaySingleMeal = (singlePhone) => {
+const displaySinglePhone = (singlePhone) => {
     const singleMealContainer = document.getElementById('phone-detail')
     singleMealContainer.textContent = '';
     const div = document.createElement('div')
