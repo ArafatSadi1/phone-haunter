dd090040a0d5d8b01fe389680790e36965d96a9b
ArafatSadi1 <arafatsadi1@gmail.com>
2022-03-02 21:01:51 +0600

    details done

--- JS/script.js
+++ JS/script.js
@@ -28,7 +28,7 @@ const displayResult = phones => {
         const div = document.createElement('div')
         div.classList.add('col')
         div.innerHTML = `
-        <div class="card m-2 p-4 h-80">
+        <div class="card m-2 p-4 h-80 rounded-3">
         <img src="${phone.image}" class="card-img-top" alt="...">
         <div class="card-body">
         <h3 class="card-title">${phone.phone_name}</h3>
@@ -48,16 +48,43 @@ const loadSingleResult = (phone) => {
 }
 
 const displaySinglePhone = (singlePhone) => {
-    const singleMealContainer = document.getElementById('phone-detail')
-    singleMealContainer.textContent = '';
-    const div = document.createElement('div')
-    div.classList.add('card')
-    div.innerHTML = `
-    <img src="${singlePhone.image}" class="card-img-top" alt="...">
-    <div class="card-body">
-      <h5 class="card-title">${singlePhone.name}</h5>
-      <p>${singlePhone.releaseDate}</p>
-    </div>
-    `
-    singleMealContainer.appendChild(div)
+    console.log(singlePhone)
+    const sensoreInfo = singlePhone.mainFeatures.sensors;
+    const senore = sensoreInfo.map(sensor => {
+        return sensor;
+    })
+        const singlePhoneContainer = document.getElementById('phone-detail')
+        singlePhoneContainer.textContent = '';
+        const div = document.createElement('div')
+        div.innerHTML = `
+        <div class="card mb-3 p-4 rounded-3">
+        <h4 class="fs-3 text-center">${singlePhone.brand}</h4>
+         <div class="row g-0">
+          <div class="col-md-4">
+           <img src="${singlePhone.image}" class="img-fluid rounded-start" alt="...">
+           <h5 class="mt-2">${singlePhone.name}</h5>
+           <p class="card-text"><small class="text-muted">${singlePhone.releaseDate}</small></p>
+          </div>
+          <div class="col-md-8">
+            <div class="card-body">
+             <p><span class="fw-bold">Memory:</span> ${singlePhone.mainFeatures.memory}</p>
+             <p><span class="fw-bold">Storage:</span> ${singlePhone.mainFeatures.storage}</p>
+             <p><span class="fw-bold">Display:</span> ${singlePhone.mainFeatures.displaySize}</p>
+             <p><span class="fw-bold">Sensore:</span> ${senore}</p>
+            </div>
+          </div>
+         </div>
+        </div>
+        `
+        singlePhoneContainer.appendChild(div)
 }
+
+/*  
+        <h3 class="text-center">Phone Details</h3>
+        <img src="${singlePhone.image}" class="card-img-top" alt="...">
+        <div class="card-body">
+        <h3 class="card-title">${singlePhone.name}</h3>
+        <p>${singlePhone.releaseDate}</p>
+        <p>Sensore: ${senore}</p>
+        </div>
+ */
